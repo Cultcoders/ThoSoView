845ee83a9663bd36d490ecc66fb1d102b90acccc
Thomas Soldan <cultcoders@users.noreply.github.com>
2022-03-17 17:56:21 +0100

    Finished Alan's Moore Tkinter Tutorial

--- alen_moore/adm_tutorial_3.py
+++ alen_moore/adm_tutorial_3.py
@@ -21,6 +21,27 @@ root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 root.configure(bg='#888')
 
+style = ttk.Style()
+# style.configure('TLabel', font='Arial 12 bold')
+# style.configure('TCheckbutton', font='Arial 14 bold', background='LightSalmon')
+# # style.configure('TRadiobutton', font="Arial 14 bold", background='LightSteelBlue')
+# style.configure('TLabelframe.Label', font='Arial 16 bold', background='PaleGreen')
+# style.configure('Status.TLabel', font='Arial 13', background='PaleGreen')
+# # dynamic style
+# style.map(
+#     'TRadiobutton',
+#     font=[('selected', 'Arial 16')]
+# )
+# style.map(
+#     'TCheckbutton',
+#     background=[
+#         ('selected', 'pink'),
+#         ('active', 'red'),
+#         ('disabled', 'green')
+#     ]
+# )
+theme_var = tk.StringVar()
+
 notebook = ttk.Notebook(root)
 notebook.grid(sticky='nesw', padx=5, pady=5)
 notebook.enable_traversal()
@@ -32,8 +53,9 @@ form_frame.columnconfigure(0, weight=1)
 form_frame.rowconfigure(5, weight=1)
 notebook.add(form_frame, text='Diary Entry', underline=0)    # underline = id char from text=
 
-dummy_frame = ttk.Frame(notebook)
-notebook.add(dummy_frame, text='Dummy Entry', underline=1)    # underline = id char from text=
+# dummy
+# dummy_frame = ttk.Frame(notebook)
+# notebook.add(dummy_frame, text='Dummy Entry', underline=1)    # underline = id char from text=
 
 # subject
 subj_frame = ttk.Frame(form_frame)
@@ -71,7 +93,7 @@ ttk.Checkbutton(
     form_frame,
     variable=private_var,
     text='Private? '
-).grid(ipadx=2, ipady=2, sticky=tk.W)
+).grid(row=2, ipadx=2, ipady=2, sticky=tk.W)
 
 # Datestamp
 datestamp_var = tk.StringVar(value='none')
@@ -92,7 +114,7 @@ datestamp_frame.grid(row=2, sticky='e', padx=3, pady=3)
 message_frame = ttk.LabelFrame(form_frame, text='Message')
 message_frame.columnconfigure(0, weight=1)
 message_frame.rowconfigure(0, weight=1)
-message_inp = tk.Text(message_frame)
+message_inp = tk.Text(message_frame, fg='navy', bg='PapayaWhip')
 message_inp.grid(sticky='nesw')
 
 scrollbar = ttk.Scrollbar(message_frame)
@@ -106,7 +128,7 @@ save_btn = ttk.Button(
     form_frame,
     text='Save'
 )
-save_btn.grid(sticky=tk.E + tk.W, ipadx=3, ipady=3)
+save_btn.grid(sticky=tk.E + tk.W)
 
 # open button
 # open_btn = tk.Button(
@@ -116,13 +138,93 @@ save_btn.grid(sticky=tk.E + tk.W, ipadx=3, ipady=3)
 # open_btn.grid(sticky=tk.W + tk.E, ipadx=3, ipady=3)
 
 # status bar
+#    style='Status.TLabel' - necessary for status bar label style
 status_var = tk.StringVar()
-tk.Label(
+ttk.Label(
     form_frame,
-    textvariable=status_var
-).grid(row=100, sticky=tk.W + tk.E, ipadx=3, ipady=3)
+    textvariable=status_var     # insert here style option
+).grid(row=100, sticky=tk.W + tk.E + tk.N + tk.S, ipadx=3, ipady=3, padx=3, pady=3)
+status_var.set('Waiting...')
 
+# seperator
+ttk.Separator(form_frame, orient=tk.HORIZONTAL).grid(sticky='ew', row=101)
+
+#***********
+# File View
+#***********
+
+files_frame = ttk.Frame(notebook)
+notebook.add(files_frame, text='Files', underline=0)
+files_frame.columnconfigure(0, weight=1)
+files_frame.rowconfigure(0, weight=1)
+
+file_tree = ttk.Treeview(files_frame)
+file_tree.grid(sticky='nesw')
+
+ft_columns = ('Name', 'Type', 'Created')
+file_tree.configure(columns=ft_columns)
+
+for heading in ft_columns:
+    file_tree.heading(heading, text=heading)
+
+file_tree.configure(show='headings')
+
+#***********************
 # Functions and Bindings
+#***********************
+
+def set_theme(*args):
+
+    theme = theme_var.get()
+    style.theme_use(theme)
+
+theme_var.trace_add('write', set_theme)
+
+def treeview_sort_column(treeview, col, reverse):
+    """ Sort a treeview column when clicked """
+    data = [
+        (treeview.set(iid, col), iid)
+        for iid in treeview.get_children('')
+    ]
+    data.sort(reverse=reverse)
+
+    for index, (sort_val, iid) in enumerate(data):
+        treeview.move(iid, '', index)
+
+    treeview.heading(
+        col,
+        command=lambda c=col: treeview_sort_column(file_tree, c, not reverse)
+    )
+
+for col in ft_columns:
+    file_tree.heading(
+        col,
+        command=lambda c=col: treeview_sort_column(file_tree, c, False)
+    )
+
+def populate_treeview(*args):
+    """ Look for txt and secret files to populate the treeview """
+    children = file_tree.get_children()
+    if children:
+        file_tree.delete(*children)
+
+    txt_files = list(Path('./test/').rglob('*.txt'))
+    sec_files = list(Path('./test/').rglob('*.secret'))
+
+    for f in (txt_files + sec_files):
+        created = datetime.fromtimestamp(f.stat().st_mtime).strftime('%Y-%m-%d')
+        file_tree.insert(
+            '',
+            tk.END,
+            iid=f.name,
+            values=(
+                f.stem,
+                f.suffix,
+                created
+            )
+        )
+populate_treeview()
+
 def weaksauce_encrypt(text, password):
     """ Weakly and insecurely encrypt some text """
     offset = sum([ord(x) for x in password])
@@ -141,15 +243,17 @@ def weaksauce_decrypt(text, password):
     )
     return decoded
 
-def open_file():
+def open_file(file_path=None):
     """ Open a file """
-    file_path = tkfd.askopenfilename(
-        title='Select a file to open',
-        filetypes=[('Secret', '*.secret'), ('Text', '*.txt')]
-    )
     if not file_path:
-        return
+        file_path = tkfd.askopenfilename(
+            title='Select a file to open',
+            filetypes=[('Secret', '*.secret'), ('Text', '*.txt')]
+        )
+        if not file_path:
+            return
 
+    file_path = './test/' + file_path
     filepath = Path(file_path)
     filename = filepath.stem
     category, subject = filename.split(' - ')
@@ -157,7 +261,8 @@ def open_file():
     if filepath.suffix == '.secret':
         password = tksd.askstring(
             'Enter password',
-            'Enter the password used to encrypt the file'
+            'Enter the password used to '
+            'encrypt the file'
         )
         # private_var.set(True) Ein kleiner Schönheitsfehler im Programm
         message = weaksauce_decrypt(message, password)
@@ -168,7 +273,7 @@ def open_file():
 
 # open_btn.configure(command=open_file)
 
-def save():
+def save(*args):
     """ Save data to a file """
     subject = subject_var.get()
     category = cat_var.get()
@@ -189,6 +294,10 @@ def save():
 
     status_var.set(f'Message was saved to {filename}')
     tkmb.showinfo('Saved', f'Message was saved to {filename}')
+    # populate_treeview()
+    root.event_generate('<<FileSaved>>')
+
+root.bind('<<FileSaved>>', populate_treeview)
 
 save_btn.configure(command=save)
 
@@ -255,8 +364,44 @@ menu.add_cascade(label='Help', menu=help_menu)
 
 size_menu = tk.Menu(options_menu, tearoff=0)
 for size in range(6, 33, 2):
-    size_menu.add_radiobutton(label=size, variable=font_size)
+    size_menu.add_radiobutton(label=str(size), variable=font_size)
+
 options_menu.add_cascade(menu=size_menu, label='Font Size')
+
+theme_menu = tk.Menu(options_menu, tearoff=0)
+options_menu.add_cascade(menu=theme_menu, label='Theme')
+
+for theme in style.theme_names():
+    theme_menu.add_radiobutton(
+        label=theme,
+        value=theme,
+        variable=theme_var
+    )
+
+#*********
+# Binds
+#+++++++++
+
+# Insert a signature into the Textfield is control-g is pressed
+def insert_signature(event):
+    message_inp.insert(tk.END, 'Tom Sol\nWizard')
+message_inp.bind('<Control-Key-g>', insert_signature)
+
+# open the selected document for m the treeview
+def on_treeclick(event):
+    selected = file_tree.selection()[0]
+    if selected:
+        open_file(selected)
+        notebook.select(0)
+file_tree.bind('<Double-Button-1>', on_treeclick)
+
+# Save document at every time
+root.bind_all('<Control-Key-s>', save)
+
+# Remove warning on status label on load from treeview
+# print(ttk.Entry().bindtags())
+root.bind_class('TEntry', '<KeyRelease>', check_filename)
+root.bind_class('TCombobox', '<KeyRelease>', check_filename)
 
 # last line
 root.mainloop()
